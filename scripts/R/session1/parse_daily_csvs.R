cf0bc7a000555450a806a6b7f5aeed22e5e7f59b
Zach Wallace <zach.wallace@live.com>
2020-08-16 12:49:13 -0700

    update parse_daily_csvs to insert new data to database, fix reported cases counts

--- scripts/R/session1/parse_daily_csvs.R
+++ scripts/R/session1/parse_daily_csvs.R
@@ -15,12 +15,12 @@ library(dplyr)
 library(ggplot2)
 
 # Choose country to look at
-country.switch <- 'France'
+country.switch <- 'US'
 
 # Set paths 
 git.path <- Sys.getenv('HOME')  # Where the base COVID19-Data-Exploration folder lives.
 
-# Pull in list of daily data. 
+# Pull in list of daily data.
 xml.path <- 'https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports'
 report.file <- download_html(xml.path)
 html.read <- read_html(report.file)
@@ -28,70 +28,88 @@ csv.list <- xml_text(html.read) %>% strsplit(split='\\n') %>% unlist
 
 # Parse html for the csv names
 dates.list <- lapply(csv.list, function(x) {
-    if (grepl(pattern='.csv', x, fixed=TRUE)) { 
+    if (grepl(pattern='.csv', x, fixed=TRUE)) {
         regex <- regexpr('\\d{2}-\\d{2}-\\d{4}.csv', x)
         return(substr(x, start=regex[[1]], stop=regex[[1]]+attr(regex, 'match.length')))
     }
-})
+}) 
 dates.list[sapply(dates.list, is.null)] <- NULL
-curr_csvs.list <- list.files(paste0(git.path, '/Code/COVID19-Data-Exploration/data/'))
+dates.list <- dates.list %>% tstrsplit(split='.csv', keep=1) %>% unlist
 
-# Fetch raw csv data
-raw.path <- 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'
-suppress <- lapply(dates.list, function(x, raw.path, curr_csvs.list) {
-    if (!(x %in% curr_csvs.list))  # Only download most recent csv
-        download.file(paste0(raw.path, x), paste0(git.path,'/Code/COVID19-Data-Exploration/data/',x))
-}, raw.path, curr_csvs.list)
+# curr_csvs.list <- list.files(paste0(git.path, '/Code/COVID19-Data-Exploration/data/'))
+# Get list of dates we do have
+source(file.path(git.path, 'Code/config_files/db_config.R'))
+con <- db_connect.fn()
+curr_dates.list <- dbGetQuery(con, 'SELECT last_update FROM covid_data.report_data') %>%
+     group_by(last_update) %>% summarize
+dbDisconnect(con)
 
-# trim whitespace and any trailing digits
-trim<- function(x) return(tstrsplit(x, "\\s|[A-Z]", keep=1) %>%unlist)
-
-# x <- dates.list[[1]]
-data.dt <- lapply(dates.list, function(x) {
-    
-    # Fix column names and standardize date formats
-    tmp.dt <- fread(paste0(git.path,'/Code/COVID19-Data-Exploration/data/',x)) %>% data.table
-    colnames(tmp.dt) <- colnames(tmp.dt) %>% gsub('[ \\/]', '_', .)  # sub spaces and slashes for underscore
-    # Fix date formatting for first 10 days
+# Force dates to be in m/d/Y to comply with JHU CSSE date formatting.  This code
+# makes me very sad :(
+curr_dates.list$last_update <- curr_dates.list$last_update %>% year %>% 
+    paste0(curr_dates.list$last_update,'-',.) %>% sub('\\d{4}-',"",.)
+# Insert data for dates we don't have into database
+missing_dates <- dates.list[!(dates.list %in% curr_dates.list$last_update)]
+if (length(missing_dates) > 0) {
+    source(file.path(git.path, 
+                     'Code/COVID19-Data-Exploration/scripts/R/session2/insert_report_data.R'),
+           local=T)
+    con <- db_connect.fn()
     tryCatch({
-        tmp.dt$Last_Update <- tmp.dt$Last_Update %>% 
-            trim %>% 
-            parse_date_time(orders=c('%m/%d/%y','%m/%d/%Y','%Y-%m-%d'))
-        return(tmp.dt)},
-        warning = function(w) {
-            message(paste('Warning!  Check file:', x))
-        },
-        error = function(e) {
-            message(paste('Error!  Check file', x))
-        }
-    )
-}) %>% rbindlist(fill=TRUE)
+        insert_report_data(con, missing_dates)  
+    }, 
+    error = function(e) {
+        print('ERROR: Database not updated!')
+        print(e)
+    })
+    dbDisconnect(con)
+}
 
-data.dt[data.dt$Recovered%>%is.na, 'Recovered'] <- 0
-data.dt[data.dt$Deaths%>%is.na, 'Deaths'] <- 0
-data.dt[data.dt$Confirmed%>%is.na, 'Confirmed'] <- 0
+# Acquire COVID data from database.  Default to most recent 3-month period
+con <- db_connect.fn()
+end.date <- Sys.Date()
+start.date <- end.date - 90
+q1 <- "SELECT * FROM covid_data.report_data"
+q2 <- paste0("WHERE last_update >= '", start.date, "'::DATE AND last_update <= '", 
+             end.date,"'::DATE")
+data.dt <- dbGetQuery(con, paste(q1, q2)) %>% data.table
+dbDisconnect(con)
 
+data.dt[confirmed%>%is.na, 'confirmed'] <- 0
+data.dt[deaths%>%is.na, 'deaths'] <- 0
+data.dt[recovered%>%is.na, 'recovered'] <- 0
 
-country_cases.dt <- data.dt[data.dt$Country_Region==country.switch,] %>% 
-    group_by(Last_Update) %>% 
-    summarize(Recovered=max(Recovered),Deaths=max(Deaths)) %>% 
+# Summarize country data and plot by case type
+country_cases.dt <- data.dt[data.dt$country_region==country.switch,] %>% 
+    group_by(last_update) %>% 
+    summarize(confirmed=sum(confirmed),
+              deaths=sum(deaths), 
+              recovered=sum(recovered)) %>% 
     data.table
 
-# Make separate data table for confirmed, since it can be an order of magnitude
-# higher than recovered/death reports.
-country_confirmed.dt <- data.dt[data.dt$Country_Region==country.switch,] %>% 
-    group_by(Last_Update) %>% 
-    summarize(Confirmed=max(Confirmed)) %>% data.table
-
-melted.dt <- melt(country_confirmed.dt, id.vars='Last_Update', variable.name = 'Cases', 
-                  value.name='Number.Reported')
-ggplot(melted.dt, aes(x=Last_Update, y=Number.Reported, color=Cases)) +
+# Confirmed
+melted.dt <- melt(country_cases.dt[,c('last_update','confirmed')], 
+                  id.vars='last_update', variable.name = 'cases', 
+                  value.name='number.reported')
+ggplot(melted.dt, aes(x=last_update, y=number.reported, color=cases)) +
     geom_point() + 
-    geom_line()
+    geom_line() +
+    ggtitle(paste("Total Confirmed Cases in", country.switch))
 
-
-melted.dt <- melt(country_cases.dt, id.vars='Last_Update', variable.name = 'Cases', 
-                  value.name='Number.Reported')
-ggplot(melted.dt, aes(x=Last_Update, y=Number.Reported, color=Cases)) +
+# Deaths
+melted.dt <- melt(country_cases.dt[,c('last_update','deaths')], 
+                  id.vars='last_update', variable.name = 'cases', 
+                  value.name='number.reported')
+ggplot(melted.dt, aes(x=last_update, y=number.reported, color=cases)) +
     geom_point() + 
-    geom_line()
+    geom_line() +
+    ggtitle(paste("Total Deaths in", country.switch))
+
+# Recovered
+melted.dt <- melt(country_cases.dt[,c('last_update','recovered')], 
+                  id.vars='last_update', variable.name = 'cases', 
+                  value.name='number.reported')
+ggplot(melted.dt, aes(x=last_update, y=number.reported, color=cases)) +
+    geom_point() + 
+    geom_line() +
+    ggtitle(paste("Total Recovered Cases in", country.switch))
--- scripts/R/session2/insert_report_data.R
+++ scripts/R/session2/insert_report_data.R
@@ -14,6 +14,7 @@ insert_report_data <- function(con, missing_dates){
   raw.path <- 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'
   
   missing_dates <- paste0(missing_dates, '.csv')
+  # x <- missing_dates[[1]]
   data.dt <- lapply(missing_dates, function(x, raw.path) {
     tmp.dt <- read.csv(url(paste0(raw.path, x)))
     colnames(tmp.dt) <- colnames(tmp.dt) %>% gsub('ï..', '', .) %>% gsub('[.|\\/]', '_', .)
@@ -22,7 +23,13 @@ insert_report_data <- function(con, missing_dates){
       tmp.dt$Last_Update <- tmp.dt$Last_Update %>% paste
       tmp.dt$Last_Update <- tmp.dt$Last_Update %>% 
         trim %>% 
-        parse_date_time(orders=c('%m/%d/%y','%m/%d/%Y','%Y-%m-%d'))
+        # parse_date_time(orders=c('%m/%d/%y','%m/%d/%Y','%Y-%m-%d'))
+        as.Date(tryFormats=c('%m/%d/%y','%m/%d/%Y','%Y-%m-%d'))
+      # Account for mis-labeled dates in the csvs
+      date.csv.name <- tstrsplit(x, '.csv', keep=1) %>% unlist %>% 
+        as.Date(tryFormats=c('%m/%d/%y','%m-%d-%Y','%Y-%m-%d'))
+      if (any(tmp.dt$Last_Update != date.csv.name))
+        tmp.dt$Last_Update <- date.csv.name
       return(tmp.dt)},
       warning = function(w) {
         message(paste('Warning!  Check file:', x))
@@ -37,8 +44,8 @@ insert_report_data <- function(con, missing_dates){
   if(grep('^lat$',names(data.dt),ignore.case=TRUE,value=FALSE) %>% length > 0){
     data.dt$Latitude <-data.dt[[grep('^lat$',names(data.dt),ignore.case=TRUE,value=TRUE)]]
   }
-  if(grep('^long$',names(data.dt),ignore.case=TRUE,value=FALSE) %>% length > 0){
-    data.dt$Longitude <-data.dt[[grep('^long$',names(data.dt),ignore.case=TRUE,value=TRUE)]]
+  if(grep('^long_$',names(data.dt),ignore.case=TRUE,value=FALSE) %>% length > 0){
+    data.dt$Longitude <-data.dt[[grep('^long_$',names(data.dt),ignore.case=TRUE,value=TRUE)]]
   }
   
   colnames(data.dt) <- colnames(data.dt) %>% tolower
@@ -46,37 +53,49 @@ insert_report_data <- function(con, missing_dates){
   table.template <- dbGetQuery(con, 'SELECT * FROM covid_data.report_data') %>% data.table %>% .[0,]
   table.template <- table.template[,-'id']
   
-  rbind(table.template, data.dt, fill=TRUE)
-  dbWriteTable(con, c('covid_data','report_data'), data.dt, append=TRUE, row.names=FALSE)
+  # # Remove old lat/lon columns in favor of better-named ones
+  # data.dt <- data.dt[, -c(grep('^lat$',names(data.dt),ignore.case=TRUE,value=TRUE),
+  #                        grep('^long_$',names(data.dt),ignore.case=TRUE,value=TRUE)
+  # ), with=FALSE]
+  # Change remote table so that incidence rate and case-fatality-ratio columns are included
+  data.dt <- data.dt[, -!(colnames(data.dt) %in% colnames(table.template)), with=FALSE]
   
+  rbind(table.template, data.dt, fill=TRUE)
+  tryCatch({
+    dbWriteTable(con, c('covid_data','report_data'), data.dt, append=TRUE, row.names=FALSE)
+    },
+    error = function(e) {
+      print('ERROR:  Table not written')
+      printe(e)
+  })
 }
-
-xml.path <- 'https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports'
-report.file <- download_html(xml.path)
-html.read <- read_html(report.file)
-csv.list <- xml_text(html.read) %>% strsplit(split='\\n') %>% unlist
-
-dates.list <- lapply(csv.list, function(x) {
-  if (grepl(pattern='.csv', x, fixed=TRUE)) { 
-    regex <- regexpr('\\d{2}-\\d{2}-\\d{4}.csv', x)
-    return(substr(x, start=regex[[1]], stop=regex[[1]]+attr(regex, 'match.length')))
-  }
-}) %>% unlist 
-dates.list <- dates.list %>% lapply(., function(x){gsub('*.csv$', '', x)}) %>% unlist
-
-# -------- Access the COVID-19 Database --------- #
-source(file.path(git.path,'Code/config_files/db_config.R'))
-con <- db_connect.fn()
-# ----------------------------------------------- #
-report_data <- dbGetQuery(con, 'SELECT last_update FROM covid_data.report_data')
-dbDisconnect(con)
-
-missing_dates <- setdiff(dates.list %>% paste, format(report_data$last_update, "%m-%d-%Y") %>% paste %>% unique)
-missing_dates <- dates.list[1:10]
-
-if(length(missing_dates) > 0){
-  insert_report_data(con, missing_dates)
-}
+# 
+# xml.path <- 'https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports'
+# report.file <- download_html(xml.path)
+# html.read <- read_html(report.file)
+# csv.list <- xml_text(html.read) %>% strsplit(split='\\n') %>% unlist
+# 
+# dates.list <- lapply(csv.list, function(x) {
+#   if (grepl(pattern='.csv', x, fixed=TRUE)) { 
+#     regex <- regexpr('\\d{2}-\\d{2}-\\d{4}.csv', x)
+#     return(substr(x, start=regex[[1]], stop=regex[[1]]+attr(regex, 'match.length')))
+#   }
+# }) %>% unlist 
+# dates.list <- dates.list %>% lapply(., function(x){gsub('*.csv$', '', x)}) %>% unlist
+# 
+# # -------- Access the COVID-19 Database --------- #
+# source(file.path(git.path,'Code/config_files/db_config.R'))
+# con <- db_connect.fn()
+# # ----------------------------------------------- #
+# report_data <- dbGetQuery(con, 'SELECT last_update FROM covid_data.report_data')
+# dbDisconnect(con)
+# 
+# missing_dates <- setdiff(dates.list %>% paste, format(report_data$last_update, "%m-%d-%Y") %>% paste %>% unique)
+# missing_dates <- dates.list[1:10]
+# 
+# if(length(missing_dates) > 0){
+#   insert_report_data(con, missing_dates)
+# }
 
 
 
