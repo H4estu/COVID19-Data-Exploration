de57fabed198b40e7cc72acacccb54219dc20912
Zach Wallace <zach.wallace@live.com>
2020-08-17 00:12:23 -0700

    Fixing up session 1 blogposts, add first for session 2

--- scripts/R/session1/parse_daily_csvs.R
+++ scripts/R/session1/parse_daily_csvs.R
@@ -36,7 +36,6 @@ dates.list <- lapply(csv.list, function(x) {
 dates.list[sapply(dates.list, is.null)] <- NULL
 dates.list <- dates.list %>% tstrsplit(split='.csv', keep=1) %>% unlist
 
-# curr_csvs.list <- list.files(paste0(git.path, '/Code/COVID19-Data-Exploration/data/'))
 # Get list of dates we do have
 source(file.path(git.path, 'Code/config_files/db_config.R'))
 con <- db_connect.fn()
@@ -56,7 +55,6 @@ if (length(missing_dates) > 0) {
            local=T)
     con <- db_connect.fn()
     tryCatch({
-<<<<<<< HEAD
         insert_report_data(con, missing_dates)  
     }, 
     error = function(e) {
@@ -80,55 +78,18 @@ data.dt[confirmed%>%is.na, 'confirmed'] <- 0
 data.dt[deaths%>%is.na, 'deaths'] <- 0
 data.dt[recovered%>%is.na, 'recovered'] <- 0
 
+
 # Summarize country data and plot by case type
 country_cases.dt <- data.dt[data.dt$country_region==country.switch,] %>% 
     group_by(last_update) %>% 
     summarize(confirmed=sum(confirmed),
               deaths=sum(deaths), 
-              recovered=sum(recovered)) %>% 
-    data.table
+              recovered=sum(recovered)) %>% data.table
 
 # Confirmed
-melted.dt <- melt(country_cases.dt[,c('last_update','confirmed')], 
-                  id.vars='last_update', variable.name = 'cases', 
+melted.dt <- melt(country_cases.dt, id.vars='last_update', variable.name = 'cases', 
                   value.name='number.reported')
 ggplot(melted.dt, aes(x=last_update, y=number.reported, color=cases)) +
-=======
-        tmp.dt$Last_Update <- tmp.dt$Last_Update %>% 
-            trim %>% 
-            parse_date_time(orders=c('%m/%d/%y','%m/%d/%Y','%Y-%m-%d'))
-        return(tmp.dt)},
-        warning = function(w) {
-            message(paste('Warning!  Check file:', x))
-        },
-        error = function(e) {
-            message(paste('Error!  Check file', x))
-        }
-    )
-}) %>% rbindlist(fill=TRUE)
-
-data.dt[data.dt$Recovered%>%is.na, 'Recovered'] <- 0
-data.dt[data.dt$Deaths%>%is.na, 'Deaths'] <- 0
-data.dt[data.dt$Confirmed%>%is.na, 'Confirmed'] <- 0
-
-
-country_cases.dt <- data.dt[data.dt$Country_Region==country.switch & 
-                                !is.na(data.dt$Admin2),] %>% 
-    group_by(Last_Update) %>% 
-    summarize(Recovered=max(Recovered),Deaths=max(Deaths)) %>% 
-    data.table
-
-# Make separate data table for confirmed, since it can be an order of magnitude
-# higher than recovered/death reports.
-country_confirmed.dt <- data.dt[data.dt$Country_Region==country.switch & 
-                                !is.na(data.dt$Admin2),] %>% 
-    group_by(Last_Update) %>% 
-    summarize(Confirmed=max(Confirmed)) %>% data.table
-
-melted.dt <- melt(country_confirmed.dt, id.vars='Last_Update', variable.name = 'Cases', 
-                  value.name='Number.Reported')
-ggplot(melted.dt, aes(x=Last_Update, y=Number.Reported, color=Cases)) +
->>>>>>> 2350afaf2e2aa949a7721bdff93f44013cf33e48
     geom_point() + 
     geom_line() +
     ggtitle(paste("Total Confirmed Cases in", country.switch))
